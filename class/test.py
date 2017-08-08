7cfbcbe9ec3601058f7f0e140e99b096067d081d
SharonHuo <374909366@qq.com>
2017-08-08 15:29:27 +0800

    Update test.py

--- class/test.py
+++ class/test.py
@@ -3,3 +3,5 @@ import media
 toy_story = media.Movie("Toy story","A story of a boy and his toys that come to life","http://upload",https://www.youtube.com)
 
 print(toy_story.storyline)
+
+toy_story.show_trailer()
