7f76e8a3a5d6fc80b60dba34f0a32ac27d4b821e
SharonHuo <374909366@qq.com>
2017-08-08 15:23:03 +0800

    Update media.py

--- class/media.py
+++ class/media.py
@@ -1,6 +1,10 @@
+import webbrowser
+
 class Movie(): #类名要首字母大写
     def __init__(self,movie_title,movie_storyline,postrer_image,trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_imaage
         self.trailer_youtube = trailer_youtube
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube)
