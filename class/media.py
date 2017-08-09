0d5155705fceaab0df915b62fe706d1d35f2d220
SharonHuo <374909366@qq.com>
2017-08-09 14:23:49 +0800

    Update media.py

--- class/media.py
+++ class/media.py
@@ -1,6 +1,8 @@
 import webbrowser
 
 class Movie(): #类名要首字母大写
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    
     def __init__(self,movie_title,movie_storyline,postrer_image,trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
